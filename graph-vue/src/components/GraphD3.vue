a70795d3db0071e07dae1fac7c599cc18427a7d5
Ziye <504250110@qq.com>
2021-03-07 17:14:25 +0800

    新功能"添加边"

--- graph-vue/src/components/GraphD3.vue
+++ graph-vue/src/components/GraphD3.vue
@@ -17,6 +17,7 @@ var nodes = [{index: 0, label: 'Node 1', groupId: 0},
 var edges = [{source: 0, target: 2},
   {source: 0, target: 1},
   {source: 1, target: 3},
+  {source: 1, target: 3},
   {source: 1, target: 4}];
 const colorList = [
   '#FCFE8B',
@@ -35,12 +36,14 @@ export default {
       width: 800,
       height:600,
 
-      node: [],
-      link: [],
+      data: {nodes: nodes, links: edges},
+      node: undefined,
+      link: undefined,
 
-      mouse: null,
+      mouse: undefined,
       mouseIsSelect: false,
-      cursor: null,
+      cursor: undefined,
+      mouseLink: undefined,
 
       isShown: false,
       selectedNode: {},
@@ -55,7 +58,7 @@ export default {
     EditNodeBox
   },
   mounted() {
-    this.initialGraph(nodes, edges);
+    this.initialGraph(this.data.nodes, this.data.links);
   },
   methods:{
     // 初始化图
@@ -75,19 +78,24 @@ export default {
           .force("y", d3.forceY())
           .on("tick", this.ticked);
 
-      this.dragger = this.drag(this.simulation);
-
       this.link = this.svg.append("g")
           .selectAll("line");
-      this.node = this.svg.append("g")
-          .selectAll("circle");
-      this.nodeText = this.svg.append("g")
-          .selectAll("text");
+      this.mouseLink = this.svg.append("g")
+          .attr("stroke", "#44cef6")
+          .attr("stroke-width", 3)
+          .attr("stroke-dasharray", 5, 10)
+          .selectAll("line");
       this.cursor = this.svg.append("circle")
           .attr("display","none")
           .attr("fill", "none")
           .attr("stroke-width", 2)
-          .attr("r", radius)
+          .attr("r", radius);
+      this.node = this.svg.append("g")
+          .selectAll("circle");
+      this.nodeText = this.svg.append("g")
+          .selectAll("text");
+
+      this.dragger = this.drag(this, this.simulation, this.mouseLink, this.data);
 
       this.updateGraph();
     },
@@ -102,13 +110,15 @@ export default {
     },
     mouseEnterNode(d) {
       this.mouseIsSelect = true;
-      this.selectedNode = nodes[d3.select(d.target).attr("index")];
+      this.selectedNode = this.data.nodes[d3.select(d.target).attr("index")];
       this.cursor.attr("display", null)
+          .attr("fill", colorList[this.selectedNode.groupId])
+          .attr("fill-opacity", 0.2)
           .attr("stroke", colorList[this.selectedNode.groupId])
           .attr("cx", this.selectedNode.x)
           .attr("cy", this.selectedNode.y)
           .transition()
-          .attr("r", 40);
+          .attr("r", radius * 2.5);
     },
     mouseLeaveNode() {
       this.mouseIsSelect = false;
@@ -125,31 +135,31 @@ export default {
     // 编辑节点信息
     Openbox(d)
     {
-       this.selectedNode = nodes[d3.select(d.target).attr("index")];
+       this.selectedNode = this.data.nodes[d3.select(d.target).attr("index")];
        this.isShown = true;
     },
     EditNode(nodeLabel){
       this.selectedNode.label = nodeLabel;
       this.isShown = false;
       this.drawNodeText();
-      console.log(nodes);
+      console.log(this.data.nodes);
       
     },
     // 节点绘制相关
     addNode(source) {
       if(this.$store.state.clickPath && this.$store.state.clickPath[0] == "0" && !this.mouseIsSelect) {
-        nodes.push({index: nodes.length, groupId: parseInt(this.$store.state.clickPath[1][2]), x: source.x, y: source.y});
-        // console.log(nodes);
+        this.data.nodes.push({index: this.data.nodes.length, groupId: parseInt(this.$store.state.clickPath[1][2]), x: source.x, y: source.y});
+        // console.log(this.data.nodes);
 
         this.drawNodes();
 
-        this.simulation.nodes(nodes);
+        this.simulation.nodes(this.data.nodes);
         this.simulation.alpha(1).restart();
       }
     },
     drawNodes() {
       this.node = this.node
-          .data(nodes)
+          .data(this.data.nodes)
           .join(
               enter => enter.append("circle").attr("r", 0)
                   .attr("fill", d => colorList[d.groupId])
@@ -167,7 +177,7 @@ export default {
     },
     drawNodeText() {
       this.nodeText = this.nodeText
-          .data(nodes)
+          .data(this.data.nodes)
           .join(
               enter => enter.append("text")
                   .attr("id", d => d.index)
@@ -179,15 +189,18 @@ export default {
           );
     },
     // 边绘制相关
-    addLink() {
+    addLink(source, targets) {
+      for (const target of targets) {
+        this.data.links.push({source, target});
+      }
       this.drawLinks();
 
-      this.simulation.force("link").links(edges);
+      this.simulation.force("link").links(this.data.links);
       this.simulation.alpha(1).restart();
     },
     drawLinks() {
       this.link = this.link
-          .data(edges)
+          .data(this.data.links)
           .join(
               enter => enter.append("line").attr("stroke", "#666").attr("stroke-width", 3).attr("stroke-opacity", opacity)
           );
@@ -198,8 +211,8 @@ export default {
 
       this.drawNodes();
 
-      this.simulation.nodes(nodes);
-      this.simulation.force("link").links(edges);
+      this.simulation.nodes(this.data.nodes);
+      this.simulation.force("link").links(this.data.links);
       this.simulation.alpha(1).restart();
     },
 
@@ -214,34 +227,67 @@ export default {
 
       this.nodeText.attr('transform', d => 'translate(' + d.x + ',' + d.y + ')');
 
-      // console.log(this.cursorNode.x);
       this.cursor.attr("cx", this.selectedNode.x)
           .attr("cy", this.selectedNode.y);
     },
-    drag(simulation) {
+    drag(self) {
+      let targetNodes = [];
       function dragStarted(event) {
-        if (!event.active) simulation.alphaTarget(0.3).restart();
+        // console.log(event);
+        if (!event.active) self.simulation.alphaTarget(0.3).restart();
         event.subject.fx = event.subject.x;
         event.subject.fy = event.subject.y;
-        simulation.force("charge", null)
+        self.simulation.force("charge", null)
             .force("link", null)
             .force("x", null)
             .force("y", null);
       }
 
       function dragged(event) {
+        // console.log(event);
         event.subject.fx = event.x;
         event.subject.fy = event.y;
+
+        targetNodes = self.data.nodes.filter(node => inRange({x: event.x, y: event.y}, node)).filter(node => linkNotExist(event.subject.index, node.index))
+        self.mouseLink = self.mouseLink
+            .data(targetNodes)
+            .join("line")
+            .attr("x1", event.x)
+            .attr("y1", event.y)
+            .attr("x2", d => d.x)
+            .attr("y2", d => d.y);
       }
 
       function dragEnded(event) {
-        if (!event.active) simulation.alphaTarget(0);
+        if (!event.active) self.simulation.alphaTarget(0);
         event.subject.fx = null;
         event.subject.fy = null;
-        simulation.force("charge", d3.forceManyBody().strength(-1000))
-            .force("link", d3.forceLink(edges).distance(100))
+        self.simulation.force("charge", d3.forceManyBody().strength(-1000))
+            .force("link", d3.forceLink(self.data.links).distance(100))
             .force("x", d3.forceX())
             .force("y", d3.forceY());
+
+        self.mouseLink = self.mouseLink
+            .data([])
+            .join("line");
+        // console.log(event.subject.index);
+        self.addLink(event.subject.index, targetNodes);
+      }
+
+      function inRange({x: sx, y: sy}, {x: tx, y: ty}) {
+        let distance = Math.hypot(sx - tx, sy - ty);
+        return distance <= radius * 3.5 && distance > radius;
+      }
+
+      function linkNotExist(source, target){
+        // console.log(source, target);
+        let notExist = true;
+        for(let i = 0, len=self.data.links.length; i < len; i++) {
+          if(self.data.links[i].source.index === source && self.data.links[i].target.index === target){
+            notExist = false;
+          }
+        }
+        return notExist;
       }
 
       return d3.drag()
@@ -249,9 +295,6 @@ export default {
           .on("drag", dragged)
           .on("end", dragEnded);
     },
-    // inRange({x: sx, y: sy}, {x: tx, y: ty}) {
-    //   return Math.hypot(sx - tx, sy - ty) <= this.minDistance;
-    // },
   },
 }
 </script>
